f014f27bcb58899261812808a2951a696e207bde
davidmfinol <davidmfinol@gmail.com>
2014-04-07 15:58:10 -0500

    Webcrawler now successfully authenticates to utdirect.
    Have to call the webcrawler with options:
    scrapy crawl utcourseguide -a username=youreid -a password=yourpassword

--- UTCourseGuide/spiders/utcourseguide_spider.py
+++ UTCourseGuide/spiders/utcourseguide_spider.py
@@ -1,15 +1,29 @@
 from scrapy.spider import Spider
+from scrapy.http import FormRequest
 
 class UtcourseguideSpider(Spider):
     name = "utcourseguide"
     allowed_domains = ["utdirect.utexas.edu"]
     start_urls = [
-        "https://utdirect.utexas.edu/ctl/ecis/results/index.WBX",
-    #	"https://utdirect.utexas.edu/ctl/ecis/results/search.WBX",
+        "https://utdirect.utexas.edu/", # login first
+        "https://utdirect.utexas.edu/ctl/ecis/results/index.WBX", # probably don't need this page
+    #	"https://utdirect.utexas.edu/ctl/ecis/results/search.WBX", # do searches from here
     #   "https://utdirect.utexas.edu/ctl/ecis/results/view_results.WBX?s_me_cis_id=2005935720000001"
     ]
     
+    username = ""
+    password = ""
+    
+    def  __init__(self, username=None, password=None, *args, **kwargs):
+        super(UtcourseguideSpider, self).__init__(*args, **kwargs)
+        self.username = username;
+        self.password = password;
+    
     def parse(self, response):
+        return [FormRequest.from_response(response,
+                    formdata={'LOGON': self.username, 'PASSWORDS': self.password},
+                    callback=self.after_login)]
+    
+    def after_login(self, response):
         print(response.body)
-        #filename = response.url.split("/")[-1]
-        #open(filename, 'wb').write(response.body)
+        
