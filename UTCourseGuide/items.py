78332f08f54819f5a673025b2960d9a7bba32276
davidmfinol <davidmfinol@gmail.com>
2014-04-23 16:17:52 -0500

    Updated to use Selenium for authentication.
    You still call it in the same manner:
    scrapy crawl utcourseguide -a username=youreid -a
    password=yourschoolpassword

--- UTCourseGuide/items.py
+++ UTCourseGuide/items.py
@@ -6,6 +6,8 @@
 from scrapy.item import Item, Field
 
 class UtcourseguideItem(Item):
+
+    # Course Survey Meta-data
     instructor = Field()
     course = Field()
     organization = Field()
@@ -13,4 +15,32 @@ class UtcourseguideItem(Item):
     semester = Field()
     formsDistributed = Field()
     formsReturned = Field()
+    
+    # The course was well organized.
+    wellOrganized = Field()
+    
+    # The instructor communicated information effectively.
+    commmunicatedEffectively = Field()
+    
+    # The instructor showed interest in the progress of students.
+    showedInterest = Field()
+    
+    # The tests/assignments were usually graded and returned promptly.
+    gradedPromptly = Field()
+    
+    # The instructor made me feel free to ask questions, disagree, and express my ideas.
+    freeToDisagree = Field()
+    
+    # At this point in time, I feel that this course will be (or has already been) of value to me.
+    courseOfValue = Field()
+    
+    # Overall, this instructor was
+    instructorWas = Field()
+    
+    # Overall, this course was
+    courseWas = Field()
+    
+    # In my opinion, the workload in this course was
+    workloadWas = Field()
+    
     pass
--- UTCourseGuide/spiders/utcourseguide_spider.py
+++ UTCourseGuide/spiders/utcourseguide_spider.py
@@ -1,29 +1,59 @@
 from scrapy.spider import Spider
 from scrapy.http import FormRequest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class UtcourseguideSpider(Spider):
+
+    # Settings for Scrapy
     name = "utcourseguide"
     allowed_domains = ["utdirect.utexas.edu"]
     start_urls = [
-        "https://utdirect.utexas.edu/", # login first
-        "https://utdirect.utexas.edu/ctl/ecis/results/index.WBX", # probably don't need this page
-    #	"https://utdirect.utexas.edu/ctl/ecis/results/search.WBX", # do searches from here
-    #   "https://utdirect.utexas.edu/ctl/ecis/results/view_results.WBX?s_me_cis_id=2005935720000001"
+        #"https://utdirect.utexas.edu/", # will require login
+        #"https://utdirect.utexas.edu/ctl/ecis/results/index.WBX", # probably don't need the home-page
+    	#"https://utdirect.utexas.edu/ctl/ecis/results/search.WBX", # do searches from here
+        "https://utdirect.utexas.edu/ctl/ecis/results/view_results.WBX?s_me_cis_id=2005935720000001" # example page
     ]
     
-    username = ""
-    password = ""
+    # Settings for Selenium
+    xpaths = {
+        'LOGON' : ".//*[@id='eid']",
+        'PASSWORDS' : ".//*[@id='pw']",
+        'submit' :   "html/body/div[1]/table[1]/tbody/tr/td[1]/table/tbody/tr[5]/td[2]/input"
+    }
     
+    
+    # Initialize scrapy and selenium
     def  __init__(self, username=None, password=None, *args, **kwargs):
         super(UtcourseguideSpider, self).__init__(*args, **kwargs)
         self.username = username;
         self.password = password;
+        self.browser = webdriver.Firefox()
     
+    
+    # Scrapy calls this on all the start-urls
     def parse(self, response):
-        return [FormRequest.from_response(response,
-                    formdata={'LOGON': self.username, 'PASSWORDS': self.password},
-                    callback=self.after_login)]
     
-    def after_login(self, response):
-        print(response.body)
+        # login with Selenium
+        self.browser.get(response.url)
+        self.browser.find_element_by_xpath(self.xpaths['LOGON']).clear()
+        self.browser.find_element_by_xpath(self.xpaths['LOGON']).send_keys(self.username)
+        self.browser.find_element_by_xpath(self.xpaths['PASSWORDS']).clear()
+        self.browser.find_element_by_xpath(self.xpaths['PASSWORDS']).send_keys(self.password)
+        self.browser.find_element_by_xpath(self.xpaths['submit']).click()
+        
+        # Click the alert message that pops-up when you log-in
+        WebDriverWait(self.browser, 3).until(EC.alert_is_present(),
+                                   'Timed out waiting for PA creation ' +
+                                   'confirmation popup to appear.')
+        alert = self.browser.switch_to_alert()
+        alert.accept()
+        
+        # Actually parse the page 
+        time.sleep(5)
+        hxs = HtmlXPathSelector(self.browser.page_source)
+        print(self.browser.page_source)
+        
         
